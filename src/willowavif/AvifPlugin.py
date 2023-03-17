a1a1e4bf59cc2d0a2495652cc7dadc349874ace4
Ben Gosney <bengosney@googlemail.com>
2023-03-17 13:59:17 +0000

    Register the image class and a converter to allow for more operations on AVIF files

--- src/willowavif/AvifPlugin.py
+++ src/willowavif/AvifPlugin.py
@@ -1,9 +1,9 @@
-import pathlib
+from pathlib import Path
 
 import pillow_avif  # noqa
 import wrapt
 from willow.image import INITIAL_IMAGE_CLASSES, ImageFile
-from willow.plugins.pillow import PillowImage
+from willow.plugins.pillow import PillowImage, _PIL_Image
 from willow.registry import registry
 
 try:
@@ -13,7 +13,7 @@ try:
     def what(wrapped, instance, args, kwargs):
         result = wrapped(*args, **kwargs)
 
-        return pathlib.Path(args[0].name).suffix[1:] if result is None else result
+        return Path(args[0].name).suffix[1:] if result is None else result
 
 except ModuleNotFoundError:
     pass
@@ -28,5 +28,16 @@ def pillow_save_avif(image, filename, quality=50, **options):
     return AVIFImageFile(filename)
 
 
+def avif_to_pillow(image_file):
+    image_file.f.seek(0)
+    image = _PIL_Image().open(image_file.f)
+    image.load()
+
+    return PillowImage(image)
+
+
+registry.register_image_class(AVIFImageFile)
 registry.register_operation(PillowImage, "save_as_avif", pillow_save_avif)
+registry.register_converter(AVIFImageFile, PillowImage, avif_to_pillow)
+
 INITIAL_IMAGE_CLASSES["avif"] = AVIFImageFile
